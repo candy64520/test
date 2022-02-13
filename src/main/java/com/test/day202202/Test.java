bb1be752a09220cc1c242bc83a0c74d57ba89cf4
candy64520 <candy64520@163.com>
2022-02-13 21:50:51 +0800

    test2022

---
+++ src/main/java/com/test/day202202/Test.java
@@ -0,0 +1,14 @@
+package com.test.day202202;
+
+/**
+ * @Package: com.test.day202202
+ * @ClassName: Test
+ * @Author: 86150
+ * @Description: 测试合并分支
+ * @Date: 2022/2/13 21:47
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("---git");
+    }
+}
