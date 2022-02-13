4c07de4ea68ed0ac40f326ec6c89fcf7a5586aeb
candy64520 <candy64520@163.com>
2022-02-13 22:12:13 +0800

    测图

---
+++ src/main/java/com/test/day202202/Test002.java
@@ -0,0 +1,15 @@
+package com.test.day202202;
+
+/**
+ * @Package: com.test.day202202
+ * @ClassName: Test002
+ * @Author: 86150
+ * @Description:
+ * @Date: 2022/2/13 22:09
+ */
+public class Test002 {
+    public static void main(String[] args) {
+        System.out.println("牛逼" +
+                "git");
+    }
+}
